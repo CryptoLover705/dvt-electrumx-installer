d6a545f9afa09a9a0677453ffd1bfa2fe1655ca4
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2021-01-23 08:05:56 +0000

    update to support ubuntu 20.04

--- distributions/Ubuntu.sh
+++ distributions/Ubuntu.sh
@@ -1,5 +1,5 @@
-if [ "$VERSION_ID" != "16.04" ] && [ "$VERSION_ID" != "18.04" ]; then
-	_warning "Only the last two LTS versions (16.04 and 18.04) are officially supported (but this will probably work)"
+if [ "$VERSION_ID" != "16.04" ] && [ "$VERSION_ID" != "18.04" ] && [ "$VERSION_ID" != "20.04" ]; then
+	_warning "Only the last three LTS versions (16.04, 18.04 and 20.04) are officially supported (but this will probably work)"
 fi
 
 . distributions/base.sh
@@ -8,6 +8,32 @@ fi
 . distributions/base-compile-rocksdb.sh
 
 if [ $(ver "$VERSION_ID") -ge $(ver "18.04") ]; then
+        newer_rocksdb=1
+fi
+
+has_rocksdb_binary=1
+
+function install_python37 {
+	$APT install -y software-properties-common || _error "Could not install package" 5
+	add-apt-repository -y ppa:deadsnakes/ppa
+	$APT update
+	packages="python3.7 python3.7-dev python3.7-distutils"
+	$APT install -y $packages || _error "Could not install package python3.7" 1
+}
+
+function binary_install_rocksdb {
+	$APT install -y librocksdb-dev liblz4-dev build-essential libsnappy-dev zlib1g-dev libbz2-dev libgflags-dev || _error "Could not install packages" 1
+}
+
+function binary_uninstall_rocksdb {
+    $APT remove  -y librocksdb-dev || _error "Could not remove rocksdb" 1
+}
+
+function install_leveldb {
+	$APT install -y libleveldb-dev || _error "Could not install packages" 1
+}
+
+if [ $(ver "$VERSION_ID") -ge $(ver "20.04") ]; then
         newer_rocksdb=1
 fi
 
